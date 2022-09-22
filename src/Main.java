7bf36b4b48d31249ffd9b29459c25eef66bf5461
EanPhillips8 <eanphillips8@gmail.com>
2022-09-22 17:37:26 -0400

    d

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String party = "I";
+        String rep = "Republican";
+        String dem = "Democrat";
+        String ind = "Independent";
+
+        System.out.println("What party do you belong to [R,D,I]: ");
+
+        if (party.equalsIgnoreCase ("R"))
+            System.out.println("Your are a " + rep);
+
+        else if (party.equalsIgnoreCase("D"))
+            System.out.println("You are a " + dem);
+
+        else if (party.equalsIgnoreCase("I"))
+            System.out.println("You are an " + ind);
+
+        else
+        {
+            System.out.println("Please enter a valid party: ");
+        }
+
     }
 }
